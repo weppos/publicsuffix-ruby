4c09912002e13d4d086ab3072d7133891df64c5a
Simone Carletti <weppos@weppos.net>
2011-03-11 22:29:07 +0100

    Standardize

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-
 require 'rubygems'
 require 'test/unit'
 require 'rr'
+
+$:.unshift File.expand_path('../../lib', __FILE__)
 require 'public_suffix_service'
 
 class Test::Unit::TestCase
