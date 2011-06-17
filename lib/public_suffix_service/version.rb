3d391f2ad384a855dccd002ceb72a72bc194f46d
Simone Carletti <weppos@weppos.net>
2011-06-17 22:48:15 +0200

    Release 0.8.4.

--- lib/public_suffix_service/version.rb
+++ lib/public_suffix_service/version.rb
@@ -12,7 +12,7 @@ module PublicSuffixService
   module Version
     MAJOR = 0
     MINOR = 8
-    PATCH = 3
+    PATCH = 4
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
