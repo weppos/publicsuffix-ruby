2ec4e382d894dee8badabae353a9ca0008142f61
Simone Carletti <weppos@weppos.net>
2016-02-05 18:40:48 +0100

    Test the PSL against the official specs
    The following 12 tests fail:
    Expected ".com" to be nil, got ".com"
    Expected ".example.com" to be nil, got "example.com"
    Expected "example.example" to be "example.example", got nil
    Expected "b.example.example" to be "example.example", got nil
    Expected "a.b.example.example" to be "example.example", got nil
    Expected "xn--85x722f.xn--55qx5d.cn" to be "xn--85x722f.xn--55qx5d.cn", got "xn--55qx5d.cn"
    Expected "www.xn--85x722f.xn--55qx5d.cn" to be "xn--85x722f.xn--55qx5d.cn", got "xn--55qx5d.cn"
    Expected "shishi.xn--55qx5d.cn" to be "shishi.xn--55qx5d.cn", got "xn--55qx5d.cn"
    Expected "xn--55qx5d.cn" to be nil, got "xn--55qx5d.cn"
    Expected "xn--85x722f.xn--fiqs8s" to be "xn--85x722f.xn--fiqs8s", got nil
    Expected "www.xn--85x722f.xn--fiqs8s" to be "xn--85x722f.xn--fiqs8s", got nil
    Expected "shishi.xn--fiqs8s" to be "shishi.xn--fiqs8s", got nil

---
+++ test/psl_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+require 'public_suffix'
+
+# This test runs against the current PSL file and ensures
+# the definitions satisfies the test suite.
+class PslTest < Minitest::Unit::TestCase
+
+  ROOT = File.expand_path("../../", __FILE__)
+
+  def self.tests
+    File.readlines(File.join(ROOT, "test/tests.txt")).map do |line|
+      line = line.strip
+      next if line.empty?
+      next if line.start_with?("//")
+      input, output = line.split(", ")
+
+      # handle the case of eval("null"), it must be eval("nil")
+      input  = "nil" if input  == "null"
+      output = "nil" if output == "null"
+
+      input  = eval(input)
+      output = eval(output)
+      [input, output]
+    end
+  end
+
+
+  def test_valid
+    # Parse the PSL and run the tests
+    defs = File.read(File.join(ROOT, "data/definitions.txt"))
+    PublicSuffix::List.default = PublicSuffix::List.parse(defs)
+
+    failures = []
+    self.class.tests.each do |input, output|
+      domain = begin
+
+
+        d = PublicSuffix.parse(input)
+        [d.sld, d.tld].join(".")
+      rescue
+        nil
+      end
+      failures << [input, output, domain] if output != domain
+    end
+
+    message = "The following #{failures.size} tests fail:\n"
+    failures.each { |i,o,d| message += "Expected %s to be %s, got %s\n" % [i.inspect, o.inspect, d.inspect] }
+    assert_equal 0, failures.size, message
+  end
+
+end
