ac3a881368c8c8ec57b5750014dab8d87cd23497
Simone Carletti <weppos@weppos.net>
2016-11-06 00:05:01 +0100

    Fix Style/MultilineIfModifier
    > Favor a normal unless-statement over a modifier clause in a multiline
    statement.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,9 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "public_suffix"
 
 Minitest::Unit::TestCase.class_eval do
-  def assert_not_equal(exp, act, msg = nil)
-    assert_operator(exp, :!=, act, msg)
-  end unless method_exists?(:assert_not_equal)
+  unless method_exists?(:assert_not_equal)
+    def assert_not_equal(exp, act, msg = nil)
+      assert_operator(exp, :!=, act, msg)
+    end
+  end
 end
