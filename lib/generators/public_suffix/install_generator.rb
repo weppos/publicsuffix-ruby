c20a716bb66138877fac2bcda9661b11b3b2795e
Kunal Tyagi <kunaltyagi12@gmail.com>
2019-07-01 21:02:33 +0530

    Update install_generator.rb

--- lib/generators/public_suffix/install_generator.rb
+++ lib/generators/public_suffix/install_generator.rb
@@ -3,30 +3,30 @@
 require 'rails/generators/base'
 
 module PublicSuffix
-	module Generators
-		class InstallGenerator <Rails::Generators::NamedBase
-			include ActiveRecord::Generators::Migration
-			source_root File.expand_path('templates',__dir__)
-			def generate_migration
-				migration_template "migration.erb", "db/migrate/create_#{table_name}.rb", migration_version: migration_version
-			end
-			def generate_model
-				invoke "active_record:model",["#{table_name}"],migration: false
-			end
-			def migration_version
-				if rails5_and_up?
-					"[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
-				end
-			end
-			def rails5_and_up?
-				Rails::VERSION::MAJOR >= 5
-			end
-			def generate_rake
-				copy_file "populate_domain_suffix.rake", "lib/tasks/populate_domain_suffix.rake"
-			end
-			def generate_configuration
-				copy_file "config.rb","config/initializers/public_suffix.rb"
-			end
-		end
-	end
+  module Generators
+    class InstallGenerator <Rails::Generators::NamedBase
+		  include ActiveRecord::Generators::Migration
+		  source_root File.expand_path('templates',__dir__)
+		  def generate_migration
+			  migration_template "migration.erb", "db/migrate/create_#{table_name}.rb", migration_version: migration_version
+		  end
+		  def generate_model
+			  invoke "active_record:model",["#{table_name}"],migration: false
+		  end
+		  def migration_version
+			  if rails5_and_up?
+				  "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+			  end
+		  end
+		  def rails5_and_up?
+			  Rails::VERSION::MAJOR >= 5
+		  end
+		  def generate_rake
+			  copy_file "populate_domain_suffix.rake", "lib/tasks/populate_#{table_name}.rake"
+		  end
+		  def generate_configuration
+			  copy_file "config.rb","config/initializers/public_suffix.rb"
+		  end
+	  end
+  end
 end
