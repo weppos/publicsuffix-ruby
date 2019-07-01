6a4b7176d724529e3fc051d059cdf7cfdfa101cd
Kunal Tyagi <kunaltyagi12@gmail.com>
2019-07-01 20:50:26 +0530

    done with the changes

--- lib/generators/public_suffix/install_generator.rb
+++ lib/generators/public_suffix/install_generator.rb
@@ -4,14 +4,14 @@ require 'rails/generators/base'
 
 module PublicSuffix
 	module Generators
-		class InstallGenerator <Rails::Generators::Base
+		class InstallGenerator <Rails::Generators::NamedBase
 			include ActiveRecord::Generators::Migration
 			source_root File.expand_path('templates',__dir__)
 			def generate_migration
-				migration_template "migration.rb", "db/migrate/create_domain_suffixes.rb", migration_version: migration_version
+				migration_template "migration.erb", "db/migrate/create_#{table_name}.rb", migration_version: migration_version
 			end
 			def generate_model
-				invoke "active_record:model",["DomainSuffix"],migration: false
+				invoke "active_record:model",["#{table_name}"],migration: false
 			end
 			def migration_version
 				if rails5_and_up?
