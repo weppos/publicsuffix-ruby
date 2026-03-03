weppos/publicsuffix-ruby

commit cdddbbc9c0dc277d8ecd8b24e8603d43870a3739

fix(build): Exclude symlink and unnecessary files
Some files, like AGENTS or RELEASING, are unnecessary for a packaged lib.

Some files, like symlinks for CLAUDE.md, can actually cause installation failures on Windows.

Fixes https://github.com/weppos/publicsuffix-ruby/issues/496

Fixes https://github.com/weppos/publicsuffix-ruby/issues/496

public_suffix.gemspec
@@ -26,7 +26,18 @@ Gem::Specification.new do |s|
   s.files = Dir.chdir(__dir__) do
     `git ls-files -z`.split("\x0").reject do |f|
       (File.expand_path(f) == __FILE__) ||
-        f.start_with?(*%w[bin/ test/ .git .rubocop Gemfile Rakefile])
+        f.start_with?(*%w[
+          bin/
+          test/
+          .git
+          .rubocop
+          Gemfile
+          Rakefile
+          AGENTS.md
+          CLAUDE.md
+          RELEASING.md
+          CONTRIBUTING.md
+      ])
     end
   end
   s.require_paths = ["lib"]

commit 7c1cf4a6ba9528eb19f83e31e7240d1a3c8cf688

Fix release crash
Introduced in cdddbbc9c0dc277d8ecd8b24e8603d43870a3739, with the exclusion of the Gemfile.

```
+ rake release
Running gem release task...
rake aborted!
Gem::MissingSpecError: Could not find 'public_suffix' (>= 2.0.2, < 8.0) among 118 total gem(s) (Gem::MissingSpecError)
Checked in 'GEM_PATH=/github/home/.gem/ruby/3.4.0:/usr/local/lib/ruby/gems/3.4.0:/usr/local/bundle' at: /usr/local/bundle/specifications/addressable-2.8.9.gemspec, execute `gem env` for more information
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:139:in 'block in Kernel#require'
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:139:in 'Monitor#synchronize'
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:139:in 'Kernel#require'
/github/workspace/Rakefile:18:in '<top (required)>'
/usr/local/bundle/gems/rake-13.3.1/exe/rake:27:in '<top (required)>'

Caused by:
Gem::MissingSpecError: Could not find 'public_suffix' (>= 2.0.2, < 8.0) among 118 total gem(s) (Gem::MissingSpecError)
Checked in 'GEM_PATH=/github/home/.gem/ruby/3.4.0:/usr/local/lib/ruby/gems/3.4.0:/usr/local/bundle' at: /usr/local/bundle/specifications/addressable-2.8.9.gemspec, execute `gem env` for more information
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:139:in 'block in Kernel#require'
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:139:in 'Monitor#synchronize'
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:139:in 'Kernel#require'
/github/workspace/Rakefile:18:in '<top (required)>'
/usr/local/bundle/gems/rake-13.3.1/exe/rake:27:in '<top (required)>'

Caused by:
LoadError: cannot load such file -- rubocop/rake_task (LoadError)
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:136:in 'Kernel#require'
<internal:/usr/local/lib/ruby/3.4.0/rubygems/core_ext/kernel_require.rb>:136:in 'Kernel#require'

public_suffix.gemspec
@@ -31,7 +31,6 @@ Gem::Specification.new do |s|
           test/
           .git
           .rubocop
-          Gemfile
           Rakefile
           AGENTS.md
           CLAUDE.md